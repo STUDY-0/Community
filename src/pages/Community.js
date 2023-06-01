0fd41745c1a256af0a2db5de0b0f0d564e021f69
hyewon <w2106@e-mirim.hs.kr>
2023-06-01 10:30:50 +0900

    post modal

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,9 +1,45 @@
+import React, { useState } from 'react';
 import logo from '../../public/logo.png';
 import styles from '../styles/Community.module.css';
 import navStyles from '../styles/nav.module.css';
 import Image from 'next/image';
 
 function Community() {
+  const [showModal, setShowModal] = useState(false);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the form submission (e.g., send data to a server)
+    // You can access the entered title and content from the title and content state variables
+    // Reset the title and content state variables
+    setTitle('');
+    setContent('');
+    // Close the modal
+    setShowModal(false);
+  };
+
+  const handleTitleChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length <= 50) {
+      setTitle(inputValue);
+    }
+  };
+
+  const handleContentChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length <= 200) {
+      setContent(inputValue);
+    }
+  };
+
+  const wordCount = content.length;
+
   return (
     <div className={styles.App}>
       <nav className={navStyles.nav}>
@@ -25,34 +61,68 @@ function Community() {
       <div className={styles['post-container']}>
         <div className={styles.newPostContainer}>
           <div className={styles.newPost}>
-              <button className={styles.newPostBtn}>POST</button>
+            <button className={styles.newPostBtn} onClick={openModal}>
+              POST
+            </button>
           </div>
         </div>
         <div className={styles.post}>
           <h1 className={styles['post-title']}>React 스터디 하실 분 ~</h1>
           <p className={styles['post-writer']}>Hyewon Yang</p>
           <p className={styles['post-date']}>2023 / 03 / 31</p>
-          <p className={styles['post-content']}>저랑 같이 react 공부해요 ㅎㅎ 저는 미림 마이스터고 출신이고 같이 공부해서 취뽀해염</p>
+          <p className={styles['post-content']}>
+            저랑 같이 react 공부해요 ㅎㅎ 저는 미림 마이스터고 출신이고 같이
+            공부해서 취뽀해염
+          </p>
         </div>
         <div className={styles.post}>
           <h1 className={styles['post-title']}>ditto</h1>
           <p className={styles['post-writer']}>newjeans</p>
           <p className={styles['post-date']}>2323 / 13 / 01</p>
-          <p className={styles['post-content']}>likeyouwantsomebody너를상상햇지항상닿아있던처음느낌그대로난</p>
+          <p className={styles['post-content']}>
+            likeyouwantsomebody너를상상햇지항상닿아있던처음느낌그대로난
+          </p>
         </div>
         <div className={styles.post}>
           <h1 className={styles['post-title']}>ditto</h1>
           <p className={styles['post-writer']}>newjeans</p>
           <p className={styles['post-date']}>2323 / 13 / 01</p>
-          <p className={styles['post-content']}>likeyouwantsomebody너를상상햇지항상닿아있던처음느낌그대로난</p>
+          <p className={styles['post-content']}>
+            likeyouwantsomebody너를상상햇지항상닿아있던처음느낌그대로난
+          </p>
         </div>
         <div className={styles.post}>
           <h1 className={styles['post-title']}>ditto</h1>
           <p className={styles['post-writer']}>newjeans</p>
           <p className={styles['post-date']}>2323 / 13 / 01</p>
-          <p className={styles['post-content']}>likeyouwantsomebody너를상상햇지항상닿아있던처음느낌그대로난</p>
+          <p className={styles['post-content']}>
+            likeyouwantsomebody너를상상햇지항상닿아있던처음느낌그대로난
+          </p>
         </div>
       </div>
+
+      {showModal && (
+        <div className={styles.modal}>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="제목"
+              value={title}
+              onChange={handleTitleChange}
+            />
+            <textarea
+              className={styles.textarea}
+              placeholder="내용"
+              value={content}
+              onChange={handleContentChange}
+            ></textarea>
+            <span className={styles.counter}>({wordCount}/200)</span><br></br>
+            <div className={styles.buttonContainer}>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
